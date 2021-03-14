7bbc79ce6e19f091f269532fc3dd681f486674d0
OLDodin <odin_sasa@mail.ru>
2021-03-14 10:22:39 +0300

    Корректная работа с тир3

--- sources/BuildFunctions.lua
+++ sources/BuildFunctions.lua
@@ -86,12 +86,20 @@ function GetTimestamp()
 end
 
 function OnStatChanged()
-	--common.LogInfo( common.GetAddonName(), 'change stat ')
-	
-	--common.UnRegisterEventHandler( OnStatChanged, "EVENT_AVATAR_STATS_CHANGED" )
 	if IsLoadingNow then
 		LoadBuildInternal( LoadingBuild )	
 	end
+end
+
+local function GetItemTier(anItemId)
+	local category = itemLib.GetCategory(anItemId)
+	local categoryInfo = category and itemLib.GetCategoryInfo(category)
+	local tier = categoryInfo and categoryInfo.sysName and string.match(categoryInfo.sysName, 'Tier(%d+)') -- sysName = 'Tier03'
+	return tier and tonumber(tier) or -1
+end
+
+local function CheckTier(anItemId)
+	return GetItemTier(anItemId) < 3
 end
 
 function LoadBuildInternal( aBuild )
@@ -102,13 +110,12 @@ function LoadBuildInternal( aBuild )
 		return false
 	end
 	local myDressedSlots = unit.GetEquipmentItemIds(avatar.GetId(), ITEM_CONT_EQUIPMENT)
-	--common.LogInfo( common.GetAddonName(), 'LoadBuild1')
+
 	for i = 0, DRESS_SLOT_UNDRESSABLE-1 do
-		if aBuild.stats[i] then
+		local dressedItemID = myDressedSlots[i]
+		if aBuild.stats[i] and CheckTier(dressedItemID) then
 			local neededOffenceStatId = aBuild.stats[i][ENUM_SpecialStatType_Offence]
 			local neededDefenceStatId = aBuild.stats[i][ENUM_SpecialStatType_Defence]
-			--common.LogInfo( common.GetAddonName(), 'LoadBuild2  '..i)
-			local dressedItemID = myDressedSlots[i]
 			local bonus = nil
 			if dressedItemID then 
 				bonus = itemLib.GetBonus(dressedItemID)
@@ -116,14 +123,13 @@ function LoadBuildInternal( aBuild )
 			if bonus and bonus.specStats then
 				local offenceStat = GetStatByType(bonus.specStats, ENUM_SpecialStatType_Offence)
 				local defenceStat = GetStatByType(bonus.specStats, ENUM_SpecialStatType_Defence)
-				--common.LogInfo( common.GetAddonName(), 'LoadBuild3')
+
 				if offenceStat and defenceStat then
 					local offenceInsignia = nil;
 					local defenceInsignia = nil;
-					--common.LogInfo( common.GetAddonName(), 'LoadBuild4')
+
 					offenceInsignia, defenceInsignia = GetInsignia()
 					if offenceInsignia then 
-					--common.LogInfo( common.GetAddonName(), 'ChangeStat offence')
 						if ChangeStat(i, offenceInsignia, dressedItemID, neededOffenceStatId, ENUM_SpecialStatType_Offence) then
 							return
 						end
@@ -132,7 +138,6 @@ function LoadBuildInternal( aBuild )
 					end
 									
 					if defenceInsignia then 
-					--common.LogInfo( common.GetAddonName(), 'ChangeStat def')
 						if ChangeStat(i, defenceInsignia, dressedItemID, neededDefenceStatId, ENUM_SpecialStatType_Defence) then
 							return
 						end
@@ -160,24 +165,20 @@ function ChangeStat(anInd, anInsignia, aDressedItemID, aNeededOffenceStatId, aTy
 	end
 	
 	local mayBeUsed = itemLib.CanUseOnItem(anInsignia, aDressedItemID)
-	--common.LogInfo( common.GetAddonName(), 'LoadBuild5')
 	if mayBeUsed then
 		if itemLib.IsUseOnItemAndTakeActions(anInsignia) then 
 			local bonus = itemLib.GetBonus(aDressedItemID)
 			local offenceStat = GetStatByType(bonus.specStats, aType)
 
 			if not aNeededOffenceStatId:IsEqual(offenceStat.id) then
-				--common.LogInfo( common.GetAddonName(), '--LoadBuild8-- ')
-				-- защита от дурака, если что то изменится в api для избежание ухода в бесконечный цикл
+				-- защита от дурака, если что то изменится в api для избежания ухода в бесконечный цикл
 				if TryFindCnt[anInd] > 100 then 
 					--common.LogInfo( common.GetAddonName(), 'terminate change by TryFindCnt[i] ')
 					IsLoadingNow = false
 					Chat(locale["limitError"])
 					return true
 				end
-
-				--common.RegisterEventHandler( OnStatChanged, "EVENT_AVATAR_STATS_CHANGED")
-
+				
 				TryFindCnt[anInd] = TryFindCnt[anInd] + 1
 				
 				avatar.UseItemOnItemAndTakeActions(anInsignia, aDressedItemID)
