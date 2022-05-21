beb1500b906a1b7040f6cccd33f4926f8f531525
OLDodin <odin_sasa@mail.ru>
2022-05-21 17:34:03 +0300

    new localization system

--- Locales/main.lua
+++ Locales/main.lua
@@ -2,22 +2,28 @@
 Global( "g_offensiveItems", {} )
 Global( "g_defensiveItems", {} )
  
+
+local localeGroup = common.GetAddonRelatedTextGroup(common.GetLocalization()) or common.GetAddonRelatedTextGroup("eng")
+
+function PrepareLocale()
+	--[[указываем инсигнии в порядке приоритета их использовния (чем раньше тем выше приоритет)]]
+	for i = 1, 12 do
+		table.insert(g_offensiveItems, common.GetAddonRelatedTextGroup("insigniaNames"):GetText("offensiveItem"..i))
+	end
+	for i = 1, 12 do
+		table.insert(g_defensiveItems, common.GetAddonRelatedTextGroup("insigniaNames"):GetText("defensiveItem"..i))
+	end
+end
+
+PrepareLocale()
+
 function getLocale()
-	return Locales[common.GetLocalization()] or Locales["eng"]
-end
-
---LogInfo(userMods.FromWString(common.GetAddonRelatedTextGroup("common"):GetText("locale")))
-
-local func, err = load(userMods.FromWString(common.GetAddonRelatedTextGroup("common"):GetText("locale")))
-if func then
-	func()
-else
-	LogInfo("Localization compilation error:", err)
-end
-
-func, err = load(userMods.FromWString(common.GetAddonRelatedTextGroup("common"):GetText("insigniaNames")))
-if func then
-	func()
-else
-	LogInfo("insigniaNames compilation error:", err)
+	return setmetatable(Locales, 
+		{__index = function(t,k) 
+			if localeGroup:HasText(k) then
+				return localeGroup:GetText(k) 
+			end
+		end
+		}
+	)
 end
