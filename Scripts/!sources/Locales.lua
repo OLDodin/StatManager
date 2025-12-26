aedc571e8c9c84666badfd717e6409dc8ee3309b
OLDodin <odin_sasa@mail.ru>
2025-12-26 20:03:15 +0300

    Добавил турецкую локализацию

--- Scripts/!sources/Locales.lua
+++ Scripts/!sources/Locales.lua
@@ -3,15 +3,14 @@ Global("LocalesInited", false)
 Global( "g_offensiveItems", {} )
 Global( "g_defensiveItems", {} )
  
-
 local localeGroup = common.GetAddonRelatedTextGroup(common.GetLocalization(), true) or common.GetAddonRelatedTextGroup("eng")
 
 function PrepareLocale()
 	--[[указываем инсигнии в порядке приоритета их использовния (чем раньше тем выше приоритет)]]
-	for i = 1, 13 do
+	for i = 1, 16 do
 		table.insert(g_offensiveItems, common.GetAddonRelatedTextGroup("insigniaNames"):GetText("offensiveItem"..i))
 	end
-	for i = 1, 13 do
+	for i = 1, 16 do
 		table.insert(g_defensiveItems, common.GetAddonRelatedTextGroup("insigniaNames"):GetText("defensiveItem"..i))
 	end
 end
